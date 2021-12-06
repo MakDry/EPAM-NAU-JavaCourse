d63b5a7f8c1c41f9c56dfa7daff6e46ffb273031
MakDry <fakecounter050@gmail.com>
2021-12-06 15:32:15 +0200

    Task 9 was added

---
+++ src/epam/nau/Task9/Matrix.java
@@ -0,0 +1,83 @@
+package epam.nau.Task9;
+
+class MatrixException extends Exception {
+    public MatrixException(String message) {
+        super(message);
+    }
+}
+
+public class Matrix {
+
+    private double[][] matrix;
+
+    public Matrix(double[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public Matrix(int rows, int columns) {
+        this.matrix = new double[rows][columns];
+    }
+
+    public double[][] matricesMultiplication(Matrix obj) {
+        checkMatrix(obj);
+        double[][] newMatrix = new double[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                newMatrix[i][j] = matrix[i][j] * obj.getIndexValue(i, j);
+            }
+        }
+        return newMatrix;
+    }
+
+    public double[][] matricesAddition(Matrix obj) {
+        checkMatrix(obj);
+        double[][] newMatrix = new double[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                newMatrix[i][j] = matrix[i][j] + obj.getIndexValue(i, j);
+            }
+        }
+        return newMatrix;
+    }
+
+    public double[][] matricesDeduction(Matrix obj) {
+        checkMatrix(obj);
+        double[][] newMatrix = new double[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                newMatrix[i][j] = matrix[i][j] - obj.getIndexValue(i, j);
+            }
+        }
+        return newMatrix;
+    }
+
+    public void setIndexValue(int row, int column, double value) {
+        matrix[row][column] = value;
+    }
+
+    public double getIndexValue(int row, int column) {
+        return matrix[row][column];
+    }
+
+    public double[][] getArray() {
+        return matrix;
+    }
+
+    public int getRows() {
+        return matrix.length;
+    }
+
+    public int getColumns() {
+        return matrix[0].length;
+    }
+
+    public void checkMatrix(Matrix obj) {
+        try {
+            if (obj.getArray().length != matrix.length || obj.getArray()[0].length != matrix[0].length) {
+                throw new MatrixException("Different sizes of matrices");
+            }
+        } catch (MatrixException e) {
+            e.getMessage();
+        }
+    }
+}
