38a6692054acdf75d677b2c620bd4597527f20fb
MakDry <fakecounter050@gmail.com>
2022-04-30 23:04:46 +0300

    Practice 6. Iterator task was completed

--- src/ua/advanced/practice6/iterator/Iterators.java
+++ src/ua/advanced/practice6/iterator/Iterators.java
@@ -1,25 +1,47 @@
-package com.epam.rd.autocode.iterator;
+package ua.advanced.practice6.iterator;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
-class Iterators {
+public class Iterators {
 
-    public static Iterator<Integer> intArrayTwoTimesIterator(int[] array){
-        throw new UnsupportedOperationException();
+    public static Iterator<Integer> intArrayTwoTimesIterator(int[] array) {
+        List<Integer> intList = new ArrayList<>();
+        for (int value : array) {
+            intList.add(value);
+            intList.add(value);
+        }
+        return intList.iterator();
     }
 
     public static Iterator<Integer> intArrayThreeTimesIterator(int[] array) {
-        throw new UnsupportedOperationException();
+        List<Integer> intList = new ArrayList<>();
+        for (int value : array) {
+            for (int i = 0; i < 3; i++) {
+                intList.add(value);
+            }
+        }
+        return intList.iterator();
     }
 
     public static Iterator<Integer> intArrayFiveTimesIterator(int[] array) {
-        throw new UnsupportedOperationException();
+        List<Integer> intList = new ArrayList<>();
+        for (int value : array) {
+            for (int i = 0; i < 5; i++) {
+                intList.add(value);
+            }
         }
-
-    public static Iterable<String> table(String[] columns, int[] rows){
-        throw new UnsupportedOperationException();
+        return intList.iterator();
     }
 
-
-
+    public static Iterable<String> table(String[] columns, int[] rows) {
+        List<String> stringList = new ArrayList<>();
+        for (String value : columns) {
+            for (int intValue : rows) {
+                stringList.add(value + intValue);
+            }
+        }
+        return () -> stringList.iterator();
+    }
 }
